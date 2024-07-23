242d95a14812c2e1f6ed8fbe884868fddf7b9687
beavazquezamat <beatriz.vazquez.amat@gmail.com>
2024-07-23 21:23:04 +0200

    guards created

--- src/app/guards/is-logged.guard.spec.ts
+++ src/app/guards/is-logged.guard.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { isLoggedGuard } from './is-logged.guard';
+import { isLoggedInGuard } from './is-logged.guard';
 
 describe('isLoggedGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => isLoggedGuard(...guardParameters));
+      TestBed.runInInjectionContext(() => isLoggedInGuard(...guardParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
