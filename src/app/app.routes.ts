e21ba5f9b9db89bfc868818b01ae162c1c048a9d
IvanBorras <ivan.borras.lopez@bootcamp-upgrade.com>
2024-07-23 19:48:09 +0200

    arreglado visualizar navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -66,7 +66,7 @@ export const routes: Routes = [
                 component: BookingsComponent
             },
             {
-                path: "vehicles",
+                path: "catalogue",
                 component: CatalogueComponent
             },
             {
