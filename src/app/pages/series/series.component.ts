01d53cd0c748128af6e1a320be53961133db0886
IvanBorras <ivan.borras.lopez@bootcamp-upgrade.com>
2024-07-26 23:22:35 +0200

    boton ventana interactiva

--- src/app/pages/series/series.component.ts
+++ src/app/pages/series/series.component.ts
@@ -6,12 +6,13 @@ import { CommonModule } from '@angular/common';
 import { FilterSeriesPipe } from '../../pipes/filter-series.pipe';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { DivisaPipe } from '../../pipes/divisa.pipe';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-series',
   standalone: true,
-  imports: [FormsModule, CommonModule, FilterSeriesPipe, DivisaPipe],
+  imports: [FormsModule, CommonModule, RouterLink ,FilterSeriesPipe, DivisaPipe],
   templateUrl: './series.component.html',
   styleUrls: ['./series.component.css']
 })
